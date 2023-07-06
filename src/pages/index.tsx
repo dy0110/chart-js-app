422d120eb45e506212837d01694da283fb9ec6bd
DaichiYoshida <daichi.yoshida@hacobu.jp>
2023-07-06 23:13:49 +0900

    ファイルにリンターを適応する

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,21 @@
-import Head from 'next/head';
-import { Inter } from '@next/font/google';
-import React from 'react';
-import Chart from 'chart.js/auto';
+import React, { useEffect } from "react";
+import Head from "next/head";
 
-const inter = Inter({ subsets: ['latin'] });
+import Chart from "chart.js/auto";
 
 export default function Home(props: any) {
-  React.useEffect(() => {
+  useEffect(() => {
     let myChart: any = null;
     const cereals = props.cereals.map((cereal: any) => {
       return { x: cereal.calories, y: cereal.carbo };
     });
     const config: any = {
-      type: 'scatter',
+      type: "scatter",
       data: {
         datasets: [
           {
-            label: '80 Cereals',
-            backgroundColor: 'rgb(255, 99, 132)',
+            label: "80 Cereals",
+            backgroundColor: "rgb(255, 99, 132)",
             data: cereals,
           },
         ],
@@ -25,7 +23,7 @@ export default function Home(props: any) {
       options: {
         plugins: {
           legend: {
-            position: 'bottom',
+            position: "bottom",
           },
         },
         scales: {
@@ -33,10 +31,10 @@ export default function Home(props: any) {
             display: true,
             title: {
               display: true,
-              text: 'Calories',
+              text: "Calories",
               font: {
                 size: 20,
-                weight: 'bold',
+                weight: "bold",
                 lineHeight: 1.2,
               },
               padding: { top: 20, left: 0, right: 0, bottom: 0 },
@@ -46,10 +44,10 @@ export default function Home(props: any) {
             display: true,
             title: {
               display: true,
-              text: 'Carbo',
+              text: "Carbo",
               font: {
                 size: 20,
-                weight: 'bold',
+                weight: "bold",
                 lineHeight: 1.2,
               },
               padding: { top: 20, left: 0, right: 0, bottom: 0 },
@@ -59,8 +57,8 @@ export default function Home(props: any) {
       },
     };
     myChart = new Chart(
-      document.getElementById('myChart') as HTMLCanvasElement,
-      config
+      document.getElementById("myChart") as HTMLCanvasElement,
+      config,
     );
     return () => {
       myChart.destroy();
@@ -75,11 +73,11 @@ export default function Home(props: any) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <section style={{ padding: '10pt' }}>
+        <section style={{ padding: "10pt" }}>
           <h1>chart-js-app</h1>
           <p>シリアルのデータ</p>
-          <div style={{ width: '400pt' }}>
-            <canvas id="myChart" width="300" height="300"></canvas>
+          <div style={{ width: "400pt" }}>
+            <canvas id="myChart" width="300" height="300" />
           </div>
         </section>
       </main>
@@ -87,8 +85,8 @@ export default function Home(props: any) {
   );
 }
 
-export async function getServerSideProps(context: any) {
-  const response = await fetch('http://localhost:3000/api/cereals');
+export async function getServerSideProps() {
+  const response = await fetch("http://localhost:3000/api/cereals");
   const cereals = await response.json();
   return {
     props: { cereals },
